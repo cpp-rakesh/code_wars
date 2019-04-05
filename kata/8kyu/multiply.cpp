f8471a72d91455769ab9aac193923bcf52388a8d
Rakesh Kumar <cpp.rakesh@gmail.com>
2019-04-06 00:27:29 +0800

    solution multiply

---
+++ kata/8kyu/multiply.cpp
@@ -0,0 +1,17 @@
+/*
+  codewars.com
+  Problem: multiply
+  06/05/2019
+*/
+
+#include <bits/stdc++.h>
+
+int multiply(int a, int b) {
+    return a * b;
+}
+
+int main() {
+    printf("%d\n", multiply(1, 2));
+
+    return 0;
+}
