175dd4993dadf79a51a5fabb70bc380b82fb940f
Rakesh Kumar <cpp.rakesh@gmail.com>
2019-04-06 00:29:32 +0800

    opposite number solution

---
+++ kata/8kyu/opposite_number.cpp
@@ -0,0 +1,17 @@
+/*
+  codewars.com
+  Problem: opposite number
+  06/05/2019
+*/
+
+#include <bits/stdc++.h>
+
+int opposite(int n) {
+    return -1 * n;
+}
+
+int main() {
+    printf("%d\n", opposite(1));
+
+    return 0;
+}
