54bd115a5b8f0ef249d3672bd4e08b4bd3e54fe7
Rakesh Kumar <cpp.rakesh@gmail.com>
2019-04-30 22:20:54 +0800

    welcome solution

---
+++ kata/8kyu/welcome.py
@@ -0,0 +1,26 @@
+d = { 'english': 'Welcome',
+      'czech': 'Vitejte',
+      'danish': 'Velkomst',
+      'dutch': 'Welkom',
+      'estonian': 'Tere tulemast',
+      'finnish': 'Tervetuloa',
+      'flemish': 'Welgekomen',
+      'french': 'Bienvenue',
+      'german': 'Willkommen',
+      'irish': 'Failte',
+      'italian': 'Benvenuto',
+      'latvian': 'Gaidits',
+      'lithuanian': 'Laukiamas',
+      'polish': 'Witamy',
+      'spanish': 'Bienvenido',
+      'swedish': 'Valkommen',
+      'welsh': 'Croeso' }
+
+def greet(l):
+        if l in d.keys():
+                return d[l]
+        else:
+                return d['english']
+
+print(greet('english'))
+print(greet('french'))
