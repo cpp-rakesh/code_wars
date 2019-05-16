a27a426def4c6b834b08bf608f8da42c68e393c3
Rakesh Kumar <cpp.rakesh@gmail.com>
2019-05-17 02:12:18 +0800

    distance from the average solution

---
+++ kata/7kyu/distance_from_the_average.py
@@ -0,0 +1,18 @@
+def distances_from_average(l):
+    a = 0
+    for i in l:
+        a = a + i
+    a = a / float(len(l))
+
+    r = []
+    for i in l:
+        r.append(round(a - i, 2))
+    return r
+
+print(distances_from_average([55, 95, 62, 36, 48]));
+print(distances_from_average([1, 1, 1, 1, 1]));
+print(distances_from_average([1, -1, 1, -1, 1, -1]));
+print(distances_from_average([1, -1, 1, -1, 1]));
+print(distances_from_average([2, -2]));
+print(distances_from_average([1]));
+print(distances_from_average([123, -65, 32432, -353, -534]));
