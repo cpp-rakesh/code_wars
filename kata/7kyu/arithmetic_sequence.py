790b048817e40b8a9f5f2db94f21d154218a4067
Rakesh Kumar <cpp.rakesh@gmail.com>
2019-05-23 07:42:57 +0800

    arithmetic sequence! solution

---
+++ kata/7kyu/arithmetic_sequence.py
@@ -0,0 +1,12 @@
+'''
+  codewars.com
+  Problem: Arithmetic Sequence!
+  23/05/2019
+'''
+
+def nthterm(a, n, d):
+    return a + n * d
+
+print(nthterm(1, 2, 3))
+print(nthterm(2, 2, 2))
+print(nthterm(-50, 10, 20))
