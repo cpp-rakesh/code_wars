1427566921e8f7698c60bf54f7e83db3e6a1e2a3
Rakesh Kumar <cpp.rakesh@gmail.com>
2020-01-10 02:52:07 +1100

    basic math (add or subtract)

--- kata/7kyu/basic_math_add_or_subtract.py
+++ kata/7kyu/basic_math_add_or_subtract.py
@@ -5,30 +5,9 @@
 '''
 
 def calculate(s):
-    temp = ''
-    for c in s:
-        if c == 'p' or c == 'm':
-            temp += ' ' + c
-        elif c == 's':
-            temp += c + ' '
-        else:
-            temp += c
-    l = temp.split()
-
-    nums = [int(n) for n in l if n.isdigit()]
-    opes = [o for o in l if o.isdigit() == False]
-
-    n = nums[0]
-    n_i = 1
-    o_i = 0
-    while (n_i < len(nums)):
-        if opes[o_i] == 'plus':
-            n += nums[n_i]
-        else:
-            n -= nums[n_i]
-        n_i += 1
-        o_i += 1
-    return str(n)
+    s = s.replace('plus', '+')
+    s = s.replace('minus', '-')
+    return str(eval(s))
 
 
 print(calculate('1plus2plus3plus4'))    # 10
