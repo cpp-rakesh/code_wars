9701eda61b5b7f80e51a13c025e1a72c6b5c7278
Rakesh Kumar <cpp.rakesh@gmail.com>
2020-03-03 00:44:05 +1100

    two oldest ages

---
+++ kata/7kyu/two_oldest_ages.py
@@ -0,0 +1,15 @@
+def two_oldest_ages(ages):
+    a = min(ages[0], ages[1])
+    b = max(ages[0], ages[1])
+
+    for i in range(2, len(ages)):
+        if ages[i] > b:
+            a = b
+            b = ages[i]
+        elif ages[i] > a:
+            a = ages[i]
+    return a,b
+
+print(two_oldest_ages([1, 5, 87, 45, 8, 8]))
+print(two_oldest_ages([6, 5, 83, 5, 3, 18]))
+print(two_oldest_ages([10, 1]))
