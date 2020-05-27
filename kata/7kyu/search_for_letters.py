de67b8126b752503915b52f6fdb2f6377fbc4f3c
Rakesh Kumar <cpp.rakesh@gmail.com>
2020-05-28 00:22:10 +1000

    search for letters

--- kata/7kyu/search_for_letters.py
+++ kata/7kyu/search_for_letters.py
@@ -5,4 +5,23 @@
 '''
 
 def change(st):
-    pass
+    d = {}
+    for c in st:
+        if c >= 'a' and c <= 'z':
+            d[ord(c) - ord('a')] = 1
+        if c >= 'A' and c <= 'Z':
+            d[ord(c) - ord('A')] = 1
+
+    r = ''
+    for i in range(26):
+        if i in d:
+            r += '1'
+        else:
+            r += '0'
+    return r
+
+if __name__ == "__main__":
+    print(change("rakesh"))
+    print(change("meenakshi"))
+    print(change("adwita"))
+    print(change("atharv"))
